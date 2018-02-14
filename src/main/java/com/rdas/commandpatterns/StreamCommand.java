c7f96d628e6464d83eb66aea37994c10ef9bd963
Rana Das <rana.pratap.das@gmail.com>
2018-02-14 20:27:34 +0000

    Cleanup

--- src/main/java/com/rdas/commandpatterns/StreamCommand.java
+++ src/main/java/com/rdas/commandpatterns/StreamCommand.java
@@ -2,8 +2,6 @@ package com.rdas.commandpatterns;
 
 import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -13,9 +11,8 @@ import java.util.stream.IntStream;
 /**
  * Created by rdas on 14/02/2018.
  */
-//@Slf4j
+@Slf4j
 public abstract class StreamCommand {
-    Logger LOG = LoggerFactory.getLogger(StreamCommand.class);
 
     int THREAD_SLEEP_MS = 500;
     int NUMBERS_COUNT = 20;
@@ -24,18 +21,18 @@ public abstract class StreamCommand {
     public void execute() {
         Stopwatch stopwatch = Stopwatch.createStarted();
         try {
-            executeInternal((value) -> LOG.info("{}: value: [{}], Elapsed: {}, ", getName(), value, stopwatch.toString()));
+            executeInternal((value) -> log.info("{}: value: [{}], Elapsed: {}, ", getName(), value, stopwatch.toString()));
         } catch (Exception e) {
-            LOG.error("Could not execute command: {}", e.getMessage(), e);
+            log.error("Could not execute command: {}", e.getMessage(), e);
         }
     }
 
     protected  String slowMappingFunction(Integer number) {
-        LOG.debug("{}: executing time consuming task on thread [{}]...", getName(), Thread.currentThread().getName());
+        log.debug("{}: executing time consuming task on thread [{}]...", getName(), Thread.currentThread().getName());
         try {
             Thread.sleep(THREAD_SLEEP_MS);
         } catch (InterruptedException e) {
-            LOG.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         }
         return String.valueOf(number);
     }
